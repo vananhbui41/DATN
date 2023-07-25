21deb2ec35c4d0793ac9ab567deeaa413725e1e3
Bui Van Anh <vananhbui41@gmail.com>
2023-07-25 18:08:07 +0700

    Trang chức năng

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,29 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Chọn Chức Năng</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="row text-center">
+                        <div class="col-sm-4">
+                            <a href="/management">
+                                <h4>Quản Lý</h4>
+                                <img width="50px" src="{{asset('images/management.png')}}"/>
+                            </a>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        <div class="col-sm-4">
+                            <a href="/cashier">
+                                <h4>Bán Hàng</h4>
+                                <img width="50px" src="{{asset('images/cashier.png')}}"/>
+                            </a>
+                        </div>
+                        <div class="col-sm-4">
+                            <a href="/report">
+                                <h4>Báo Cáo</h4>
+                                <img width="50px" src="{{asset('images/dashboard.png')}}"/>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
